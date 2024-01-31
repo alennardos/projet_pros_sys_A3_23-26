16eebd806601bebc44f9a3a8754f1562f25f6648
Antoine <antflaudedu@gmail.com>
2024-01-31 14:25:29 +0100

    Create class "Class2"
    Fonctions :
    - direBonjour();

---
+++ src/Class2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Application_Test_1.src
+{
+    internal class Class2
+    {
+
+        public void direBonjour()
+        {
+            Console.WriteLine("Bonjour");
+        }
+
+    }
+}
