b29e62ee9cd8545cba35fadef06593821de6ffd4
Mahraoui <adam.mahraoui@epita.fr>
2024-02-06 16:52:35 +0100

    création menue 1

--- src/VueModeleSave.cs
+++ src/VueModeleSave.cs
@@ -1,9 +1,11 @@
 ﻿using ConsoleApp1.src;
+using ConsoleApp1.src.SaveType;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Resources;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,55 +18,135 @@ namespace Console_Application_Test_1.src
         private Vue vueobject;
         private Sauvegardes saves;
         private String errorArgument = "Erreur CS8604 : argument invalide.";
+        private bool run;
+
+        
 
         public VueModeleSave(String str)
         {
+            this.run = true;
             this.vueobject = new Vue();
             this.saves = new Sauvegardes();
             this.rm = new ResourceManager("ConsoleApp1.languages." + str, Assembly.GetExecutingAssembly());
             Console.WriteLine(vueobject);
         }
-        
+
         public void menu()
         {
-            List<String> list = ["HOME_create_save", "HOME_lunch_save", "HOME_edit_save", "HOME_settings"];
 
-            this.vueobject.SetOutPut(this.rm.GetString("HOME_hello")+"\n");
+            List<String> list = ["HOME_create_save", "HOME_lunch_save", "HOME_edit_save", "HOME_settings", "quit"];
+
+            this.vueobject.SetOutPut(this.rm.GetString("HOME_hello") + "\n");
             this.vueobject.afficher();
 
-            for (int i = 0; i <list.Count; i++)
+
+            while (this.run)
             {
-                this.vueobject.SetOutPut(i+1 + ") " + this.rm.GetString(list[i]));
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                this.vueobject.SetOutPut(i + 1 + ") " + this.rm.GetString(list[i]));
                 this.vueobject.afficher();
             }
 
-            this.userInput = this.vueobject.GetInput();
+                this.userInput = this.vueobject.GetInput();
 
-            if (this.userInput.Equals("1"))
-            {
-                creerSauvegarde();
-            }
-            else if (this.userInput.Equals("2"))
-            {
-                modifierSauvegarde();
-            }
-            else if (this.userInput.Equals("3"))
-            {
-                effectuerSauvegarde();
-            }
-            else if(this.userInput.Equals("4"))
-            {
-                assignerParametres();
-            }
-            else
-            {
-                vueobject.SetOutPut(rm.GetString("enter_bad") ?? errorArgument);
+
+                switch (this.userInput)
+                {
+                    case "1":
+                        creerSauvegarde();
+                        break;
+
+                    case "2":
+                        modifierSauvegarde();
+                        break;
+
+                    case "3":
+                        effectuerSauvegarde();
+                        break;
+
+                    case "4":
+                        assignerParametres();
+                        break;
+
+                    case "5":
+                            this.run = false;
+                        break;
+
+                    default:
+                        vueobject.SetOutPut(rm.GetString("enter_bad") ?? errorArgument);
+                        vueobject.afficher();
+                        break;
+                }
             }
         }
 
         public void creerSauvegarde()
-        { 
+        {
+            
             vueobject.SetOutPut(rm.GetString("HOME_create_save") ?? errorArgument);
+            List<String> list = ["CREATE_name_save", "CREATE_source_save", "CREATE_destination_save", "CREATE_type_save"];
+            this.vueobject.SetOutPut(this.rm.GetString("home"));
+
+            this.vueobject.afficher();
+            string name = "";
+            string src = "";
+            string dst = "";
+            TypeSave type = null;
+            
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                this.vueobject.SetOutPut(i + 1 + ") " + this.rm.GetString(list[i]));
+                this.vueobject.afficher();
+                this.userInput = this.vueobject.GetInput();
+
+                if (this.userInput == "")
+                {
+                    i--;
+                    vueobject.SetOutPut(rm.GetString("enter_bad") ?? errorArgument);
+                    vueobject.afficher();
+
+                }
+                
+                switch (i)
+                {
+                    case 0:
+                        name = this.userInput; break;
+
+                    case 1:
+                        src = this.userInput;break;
+                    case 2:
+                        dst = this.userInput; break;
+                    case 3:
+                        if (this.userInput == "1")
+                            type = new SaveComplete();
+                        else if (this.userInput == "2")
+                            type = new SaveDif();
+                        else
+                        {
+                            i--;
+                            vueobject.SetOutPut(rm.GetString("enter_bad") ?? errorArgument);
+                            vueobject.afficher();
+                        }
+                        break;
+                }
+                
+            }
+
+            if (this.saves.createSave(name, src, dst, type))
+            {
+                vueobject.SetOutPut(rm.GetString("CREATE_succes"));
+                this.vueobject.afficher();
+            }
+            else
+            {
+                vueobject.SetOutPut(rm.GetString("CREATE_fail"));
+                this.vueobject.afficher();
+            }
+
+
         }
 
         public void effectuerSauvegarde()
