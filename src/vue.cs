1583061854dee72178409b67324fb942efdef25c
Mahraoui <adam.mahraoui@epita.fr>
2024-02-06 09:52:49 +0100

    debut fichier vue

---
+++ src/vue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.src
+{
+    internal class Vue
+    {
+        private string outPut;
+
+        private static string GetInput()
+        {
+            string str = Console.ReadLine();
+            if (str == null || str.Length == 0)
+            {
+                //Console.WriteLine("");
+                return "";
+            }
+            else
+                return str;
+        }
+
+        private void SetOutPut()
+        {
+
+        }
+
+        private void afficher()
+        {
+            Console.WriteLine("");
+        }
+
+    }
+}
