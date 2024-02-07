77a62101ee583c49b1b6b6f7acab65b5ad68d4fd
Mahraoui <adam.mahraoui@epita.fr>
2024-02-07 09:46:34 +0100

    debut fct save complete

--- src/SaveType/SaveComplete.cs
+++ src/SaveType/SaveComplete.cs
@@ -10,7 +10,33 @@ namespace ConsoleApp1.src.SaveType
 
         public void save(FileInfo file, string targetFilePath)
         {
-            Console.WriteLine("TODO");
+            try
+            {
+                if (File.Exists(targetFilePath))
+                {
+                    try
+                    {
+                        string tempFilePath = Path.Combine(Path.GetTempPath(), "tempfile");
+                        file.CopyTo(tempFilePath, true);
+                        Console.WriteLine("copie dans fichier temp réussit");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("erreur de copie dans temp");
+                    }
+
+                    File.Delete(targetFilePath);
+                    file.CopyTo(targetFilePath);
+                }
+
+                else
+                    file.CopyTo(targetFilePath);
+            }
+
+            catch (Exception ex)
+            {
+                //erreur
+            }
         }
     }
 }
