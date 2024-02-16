5c8e2132d5497b295a960d7488c4810d4bafd6d7
Mahraoui <mahraoui30@gmail.com>
2024-02-16 10:04:14 +0100

    page edition de sauvegarde

---
+++ src/vues/EditSave.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1.src.vues
+{
+    /// <summary>
+    /// Logique d'interaction pour EditSave.xaml
+    /// </summary>
+    public partial class EditSave : Window
+    {
+        public EditSave()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Back to home
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
