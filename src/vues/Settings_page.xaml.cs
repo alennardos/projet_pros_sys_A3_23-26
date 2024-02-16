8b48207535e7442c2c1f57831d1d398ceef708b5
Antoine <antflaudedu@gmail.com>
2024-02-16 16:15:13 +0100

    changed settings menu from window to page
    changed settings menu from window to page

--- src/vues/Settings_page.xaml.cs
+++ src/vues/Settings_page.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApp1
 {
     /// <summary>
-    /// Logique d'interaction pour Settings_menu.xaml
+    /// Logique d'interaction pour Settings_page.xaml
     /// </summary>
-    public partial class Settings_menu : Window
+    public partial class Settings_page : Page
     {
-        public Settings_menu()
+        public Settings_page()
         {
             InitializeComponent();
         }
