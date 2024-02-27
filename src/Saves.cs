77cf3bdcce812bb0a43271e281137e56498ba511
Mahraoui <mahraoui30@gmail.com>
2024-02-27 09:56:21 +0100

    modification fonction remove pour suppression sans index

--- src/Saves.cs
+++ src/Saves.cs
@@ -7,6 +7,7 @@ using System.Reflection.PortableExecutable;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Xml;
 using System.Xml.Linq;
 
@@ -178,6 +179,12 @@ namespace ConsoleApp1.src
             {
                 this.saves.RemoveAt(num);
             }
+        }
+
+        //Remove a save without index
+        public void removeSave(Save save)
+        {
+                this.saves.Remove(save);
         }
 
         // Change a save parameter about typeSave
