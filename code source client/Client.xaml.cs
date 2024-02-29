ae1c34d7741e7f741e679a9dd103c094b9690761
alen amirbekyan <alenamirbekyan18@gmail.com>
2024-02-29 20:14:48 +0100

    coude source client (definie dans un autre projet)

---
+++ code source client/Client.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace clientProgress
+{
+    /// <summary>
+    /// Logique d'interaction pour Client.xaml
+    /// </summary>
+    public partial class Client : Window
+    {
+
+        Socket s;
+        bool clickPlay;
+
+
+        public void getValue(object sender, DoWorkEventArgs e)
+        {
+            byte[] value = new byte[128];
+            bool run = true;
+            int progress = 0;
+            while (run)
+            {
+                if (clickPlay)
+                {
+                    s.Send(Encoding.UTF8.GetBytes("PlayPause"));
+                    this.clickPlay = false;
+                }
+                else
+                {
+                    s.Send(Encoding.UTF8.GetBytes("normale"));
+                }
+                s.Receive(value);
+                progress = BitConverter.ToInt32(value, 0);
+                (sender as BackgroundWorker).ReportProgress(progress);
+                run = progress != 100;
+            }
+            //TODO : demander Adam comment fermer la fenetre
+        }
+
+        public void setPlayPause()
+        {
+            this.clickPlay = true;
+        }
+
+        public Client(Socket s)
+        {
+            this.s = s;
+            this.clickPlay = false;
+            InitializeComponent();
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += getValue;
+            worker.ProgressChanged += worker_ProgressChanged;
+            worker.RunWorkerAsync();
+        }
+
+        void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //initialisation de la barre de progression avec le pourcentage de progression
+            pbstatus1.Value = e.ProgressPercentage;
+
+            //Affichage de la progression sur un label
+            lb_etat_prog_server.Content = pbstatus1.Value.ToString() + "%";
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.setPlayPause();
+        }
+
+
+    }
+}
---
+++ code source client/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace clientProgress
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(ip.Text), Int32.Parse(port.Text));
+                s.Connect(ipep);
+                EcouterReseau(s);
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show("Error\n" + ex);
+            }
+        }
+
+        private static void EcouterReseau(Socket s)
+        {
+            byte[] buf = new byte[4];
+            s.Receive(buf);
+            Client c = new Client(s);
+            c.Show();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
